9b4a5dd47e20191d1a9547967f4f0d84c074a2b5
henrycky <henrycky_wottikosky@hotmail.com>
2023-11-28 21:08:49 -0300

    Initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,10 +3,27 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
+const data = [
+  {
+    id: '1',
+    name: 'Posto ipiranga',
+  },
+  {
+    
+  },{
+    
+  },{
+    
+  },{
+    
+  },
+]
+
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Mancomunado</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
