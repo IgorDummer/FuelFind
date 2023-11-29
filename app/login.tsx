c6e089ab95362bd38b5f73bec738b1cb38ed0ceb
JuliaCald <j.caldeira.j@gmail.com>
2023-11-29 17:21:03 -0300

    Co-authored-by: Henrycky Wottikosky <HenryWotti@users.noreply.github.com> Co-authored-by: Igor Dummer <IgorDummer@users.noreply.github.com>

--- app/login.tsx
+++ app/login.tsx
@@ -1,34 +1,81 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { TouchableOpacity, Image, TextInput, Button, StyleSheet } from 'react-native';
+import { Text, View } from '../components/Themed';
 
-export default function LoginForm(){
+export default function TabTwoScreen() {
+  return (
     <View style={styles.container}>
-    <TextInput
-      style={styles.input}
-      placeholder="Email"
-    />
-    <TextInput
-      style={styles.input}
-      placeholder="Password"
 
-    />
-    <Button title="Login" />
-  </View>
-};
+      <View style={styles.container_title}>
+        <Image
+          source={require ('../assets/images/white_local_gas_station.png')}
+          //resizeMode="contain"  
+        ></Image>
+        <Text>
+          FuelFind
+        </Text>
+      </View>
+     
+      <View style={styles.container_login}>
+        <TextInput style={styles.input}
+          placeholder='Usuário ou e-mail'
+        />
+        <TextInput style={styles.input}
+          placeholder='Senha'
+        />
+        <Button 
+          title = 'Entrar'      
+          color={'#122209'} 
+          //onPress={handleLogin}
+        />
+
+        <TouchableOpacity>
+          <Text style={styles.text}>Esqueci minha senha</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.text}>Criar conta</Text>
+        </TouchableOpacity>
+
+        <View lightColor="#1B4203" darkColor="#1B4203" />
+      </View>
+      
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: '#f12aff',
-  color: '#b31d0f',
-},
-input: {
-  width: '80%',
-  marginBottom: 15,
-  borderWidth: 1,
-  padding: 10,
-  color: '#ffffff'
-},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#1B4203",
+  },
+  container_title: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#1B4203",
+  },
+  container_login: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: "#1B4203",
+  },
+  input: {
+    width: '150%',
+    marginBottom: 15,
+    borderWidth: 1,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    padding: 5,
+    color: 'white',
+    borderBottomWidth: 1.5, // Adiciona uma linha embaixo do TextInput
+    borderBottomColor: 'white', // Cor da linha
+  },
+  text: {
+    marginTop: 20
+  }
 });
---
+++ react-native.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    assets: ['./assets/fonts/'], // Caminho para a pasta de fontes
+  };
