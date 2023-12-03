816d09554e0011cfbbd0159f5cf217cf76a9c834
Igor Dummer <igorwdummer@outlook.com>
2023-12-03 16:40:29 -0300

    feat: ✨ Adiciona modal de tipo de combustível
    Co-authored-by: Henrycky Wottikosky <HenryWotti@users.noreply.github.com>
    Co-authored-by: Julia <julia-caldeira@users.noreply.github.com>

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,130 @@
-import { StyleSheet } from 'react-native';
-import EditScreenInfo from '../../components/EditScreenInfo';
+import React, { useState } from 'react';
+import { StyleSheet, Image, Modal, TouchableWithoutFeedback, ScrollView } from 'react-native';
+import { Searchbar, Checkbox, Button } from 'react-native-paper';
 import { Text, View } from '../../components/Themed';
+import enumFuelTypes from '../../utils/enumFuelTypes';
 
 export default function TabOneScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedFuelTypes, setSelectedFuelTypes] = useState<string[]>([]);
+
+  const openModal = () => setModalVisible(true);
+  const closeModal = () => setModalVisible(false);
+
+  const handleCheckboxToggle = (fuelType: string) => {
+    if (selectedFuelTypes.includes(fuelType)) {
+      setSelectedFuelTypes(selectedFuelTypes.filter((type) => type !== fuelType));
+    } else {
+      setSelectedFuelTypes([...selectedFuelTypes, fuelType]);
+    }
+  };
+
+  const handleFilter = () => {
+    console.log('Tipos de combustíveis selecionados:', selectedFuelTypes);
+    closeModal();
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Mancomudamunado Sim</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={styles.container_title}>
+        <Image source={require('../../assets/images/green_gas_station.svg')} />
+        <Text style={styles.title}>FuelFind</Text>
+      </View>
+      <Text style={styles.title}>POSTOS PRÓXIMOS</Text>
+
+      <TouchableWithoutFeedback onPress={openModal}>
+        <View style={styles.searchBar}>
+          <Text style={styles.searchText}>Pesquisa por tipo de combustível</Text>
+        </View>
+      </TouchableWithoutFeedback>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalHeaderText}>Selecione ao menos um tipo de combustível para pesquisar</Text>
+
+            {Object.values(enumFuelTypes).map((fuelType) => (
+              <View key={fuelType} style={styles.checkboxContainer}>
+                <Checkbox
+                  status={selectedFuelTypes.includes(fuelType) ? 'checked' : 'unchecked'}
+                  onPress={() => handleCheckboxToggle(fuelType)}
+                />
+                <Text style={styles.checkboxLabel}>{fuelType}</Text>
+              </View>
+            ))}
+
+            <Button mode="contained" onPress={handleFilter}>
+              Filtrar
+            </Button>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  container_title: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    paddingTop: 10,
+    paddingBottom: 10,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: 'black',
+    paddingVertical: 15,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  searchBar: {
+    margin: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '90%',
+    borderRadius: 15,
+  },
+  searchText: {
+    fontSize: 16,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 16,
+    maxHeight: '60%',
+  },
+  modalHeaderText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  checkboxLabel: {
+    marginLeft: 10,
+    fontSize: 16,
   },
 });
---
+++ utils/enumFuelTypes.tsx
@@ -0,0 +1,10 @@
+const enumFuelTypes = {
+  GASOLINA_COMUM: 'Gasolina Comum',
+  GASOLINA_ADITIVADA: 'Gasolina Aditivada',
+  DIESEL_S10: 'Diesel S10',
+  DIESEL_S500: 'Diesel S500',
+  GNV: 'GNV',
+  ETANOL: 'Etanol',
+}
+
+export default enumFuelTypes;
